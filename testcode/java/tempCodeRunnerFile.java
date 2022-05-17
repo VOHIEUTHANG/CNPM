22715380398455c633aa03c96a366c830167e2f8
ducmanhlai <ducmanhlai22@gmail.com>
2022-05-17 22:59:15 +0700

    chua biet

--- testcode/java/tempCodeRunnerFile.java
+++
@@ -1,7 +0,0 @@
-import java.io.File;
-import java.io.IOException;
-public class readfile {
-   public static void main(String args[]) throws IOException {
-      File f1 = new File("..\\..\\..\\config.properties");
-      }
-   }
